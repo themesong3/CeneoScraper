53007e74d29dc3d4adc159af504588b7c509771d
themesong3 <229915@student.uek.krakow.pl>
2023-03-25 17:41:15 +0100

    2023.03.25 AOK

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import requests
+#product_code = input("Podaj kod produktu")
+product_code = "100361771"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+
+response = requests.get(url)
+print(response.status_code)
+
